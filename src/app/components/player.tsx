763526c9cc7232a59fb5887c8046ec97de13b761
SAWKIT <potions.plaquiste0l@icloud.com>
2025-01-18 23:44:15 +0100

    Player Added

--- src/app/components/player.tsx
+++ src/app/components/player.tsx
@@ -3,9 +3,11 @@ export default function VideoPlayer() {
         <div className="flex justify-center items-center max-h-screen bg-black w-full max-w-3xl">
             <video
             className="w-full max-w-3xl"
+            playsInline
+            muted={false}
             controls
             >
-            <source src="https://filedn.eu/lWsEldFrD0ljPhHINdv6TVJ/media/Films/L'effet%20Papillon.avi" type="video/avi" />
+            <source src="https://filedn.eu/lWsEldFrD0ljPhHINdv6TVJ/media/Films/L'effet%20Papillon.mp4" type="video/mp4" />
             Votre navigateur ne supporte pas le format vidéo.
             </video>
         </div>
